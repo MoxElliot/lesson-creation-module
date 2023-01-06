0ead140a981979b3278e69aa2b7a9b1300777dd0
MoxElliot <eric0952@gmail.com>
2023-01-05 18:37:38 -0800

    removed additional unnecessary code and stylesheets

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import '../styles/index.scss';
-import '../styles/instructorCal.scss';
 import '../styles/instructorDash.scss';
-import '../styles/studentCal.scss';
 import { Provider } from "react-redux";
 import { store } from "../redux/store"
 
